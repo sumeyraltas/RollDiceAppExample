b3aebd18fb0bcde210b979c8cce800f7acfd11f8
sumeyraltas <sumeyraltas@gmail.com>
2023-11-05 23:23:08 +0300

    basic roll dice app example,  rolling effect can be added to dice for improvement, background color can be changed.

--- RollDiceAppExample/ContentView.swift
+++ RollDiceAppExample/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diceCount: Int = 1
+    @State private var diceRolls: [Int] = [1]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            Color(red: 0.0 / 255.0, green: 51.0 / 255.0, blue: 25.0 / 255.0)
+                .edgesIgnoringSafeArea(.all)
+            
+            Spacer()
+            
+            VStack {
+                Spacer()
+                HStack {
+                    ForEach(0..<min(diceCount, 3), id: \.self) { index in
+                        Image("\(diceRolls[index])Dice")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .padding()
+                    }
+                    
+                }
+                if diceCount > 3 {
+                    HStack {
+                        ForEach(3..<diceCount, id: \.self) { index in
+                            Image("\(diceRolls[index])Dice")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100, height: 100)
+                                .padding()
+                        }
+                    }
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                VStack {
+                    Button(diceCount == 1 ? "Roll the Dice" : "Roll the Dices") {
+                        diceRolls = (0..<diceCount).map { _ in
+                            Int.random(in: 1...6)
+                        }
+                    }
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+                    
+                    HStack{
+                        if diceCount > 1 {
+                            Button("-") {
+                                diceCount -= 1
+                                diceRolls.removeLast()
+                            }
+                            .font(.title)
+                            .frame(width: 64, height: 45)
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            
+                        }
+                        
+                        if diceCount < 6 {
+                            Button("+") {
+                                diceCount += 1
+                                diceRolls.append(1)
+                            }
+                            .font(.title)
+                            .frame(width: 64, height: 45)
+                            .background(Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
